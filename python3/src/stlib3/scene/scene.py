60868cf56639fb5e27749db1f0ee672323f89c0e
bakpaul <paul.baksic@outlook.fr>
2026-06-11 11:43:52 +0200

    Fix requiredPlugins

--- python3/src/stlib3/scene/scene.py
+++ python3/src/stlib3/scene/scene.py
@@ -48,8 +48,8 @@ def Scene(root, gravity=[0.0,-9.81,0.0],
             if "SofaPython3" not in plugins:
                 plugins.append("SofaPython3")
 
-            if "Sofa.Component.ODESolver.Backward":
-                plugins.append("Sofa.Component.ODESolver.Backward")
+            if "Sofa.Component.IntegrationSchemes.Backward":
+                plugins.append("Sofa.Component.IntegrationSchemes.Backward")
 
             if doDebug:
                 from splib3.debug import DebugManager
